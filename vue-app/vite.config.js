991d8c7f637a185d9d5db1cef96b3b6e0038580e
siddharthsaraf <smsarraf@gmail.com>
2026-07-11 01:22:56 +0530

    Social Login.

--- vue-app/vite.config.js
+++ vue-app/vite.config.js
@@ -10,4 +10,12 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    host: '0.0.0.0',
+    port: 3001,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 3001,
+  },
 })
