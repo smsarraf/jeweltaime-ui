8f0d2f1674ca2d03fb0ea2190e7adf1cbf35a546
siddharthsaraf <smsarraf@gmail.com>
2026-07-20 18:42:29 +0800

    Site Map and

--- vue-app/src/pages/ProductList.vue
+++ vue-app/src/pages/ProductList.vue
@@ -237,7 +237,7 @@ async function fetchProducts() {
         params: {
           page: currentPage.value,
           size: pageSize,
-          sort: [sortBy.value]
+          sort: sortBy.value
         }
       })
     }
